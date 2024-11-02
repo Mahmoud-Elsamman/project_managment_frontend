8e086ad4dd345dca37561b6bbbb06f1f9f4aae3e
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2024-11-02 21:53:51 +0200

    feat: implement api utility method

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
+import { getToken } from "../services/authService";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://localhost:5274/api",
 });
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = getToken();
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
